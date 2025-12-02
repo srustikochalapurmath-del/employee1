4cfe8e1b162b0e661ff58addefadb20598cfff63
srustikochalapurmath <srustikochalapurmath@gmail.com>
2025-12-02 10:48:43 +0530

    new

---
+++ test_emp1.py
@@ -0,0 +1,9 @@
+from emp import employee details
+from test employee_details ():
+    expected output = (
+     " Employee_Name : Alice\n"
+     "Employee_id : E1101\n"
+     "Department : IT"
+     "Salary : 55000"
+    )
+    assert employee_details("Alice","E1101","IT",55000) == expected output 
